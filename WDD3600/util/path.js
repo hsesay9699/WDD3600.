893466ebe4858fec968c68f744f05be20e03411c
hsesay9699 <hsesay969@gmail.com>
2026-01-31 00:12:55 -0500

    Delete WDD3600 directory

--- WDD3600/util/path.js
+++
@@ -1,8 +0,0 @@
-/**
- * util/path.js
- * Helper to get the absolute path to the project root directory.
- * This is used when building absolute paths for files (views, data files, etc.).
- */
-const path = require('path');
-
-module.exports = path.dirname(require.main.filename);
